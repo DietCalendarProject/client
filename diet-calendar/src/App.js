5c52132dff4b872c65007418881d0f0a0b2ba625
Jun <118579021+jun42@users.noreply.github.com>
2023-09-01 15:17:12 +0900

    feat : 리액트 라우터를 사용해서 페이지 나눔
    일단 스케치 형식으로 폴더 나누고 페이지 경로 나눴습니다.

--- diet-calendar/src/App.js
+++ diet-calendar/src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
+import { Main } from "./Main/Main";
+import { Login } from "./Login/Login";
+import { SignUp } from "./SignUp/SignUp";
+import { Calendar } from "./Calendar/Calendar";
+import { MyPage } from "./MyPage/MyPage";
+import { ProjectDescription } from "./ProjectDescription/ProjectDescription";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Main />} exact={true}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/signup" element={<SignUp />}></Route>
+      <Route path="/calendar" element={<Calendar />}></Route>
+      <Route path="/my-page" element={<MyPage />}></Route>
+      <Route
+        path="/project-description"
+        element={<ProjectDescription />}
+      ></Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ diet-calendar/src/ProjectDescription/ProjectDescription.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ProjectDescription = () => {
+  return <div>ProjectDescription</div>;
+};
