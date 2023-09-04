68ed683074763660da09f3441847aa31e3b9f31c
denshou <132555762+denshou@users.noreply.github.com>
2023-09-04 15:41:35 +0900

    캘린더 추가

---
+++ src/pages/Cells.js
@@ -0,0 +1,70 @@
+import React from "react";
+import {
+  format,
+  endOfMonth,
+  endOfWeek,
+  isSameDay,
+  startOfMonth,
+  startOfWeek,
+  isSameMonth,
+  parse,
+  addDays,
+} from "date-fns";
+
+import "./Cells.css";
+
+const Cells = (props) => {
+  const monthStart = startOfMonth(props.currentDate);
+  const monthEnd = endOfMonth(monthStart);
+  const startDate = startOfWeek(monthStart);
+  const endDate = endOfWeek(monthEnd);
+
+  const rows = [];
+  let days = [];
+  let day = startDate;
+  let formattedDate = "";
+
+  while (day <= endDate) {
+    for (let i = 0; i < 7; i++) {
+      formattedDate = format(day, "d");
+      const cloneDay = day;
+      days.push(
+        <div
+          className={`col cell ${
+            !isSameMonth(day, monthStart)
+              ? "disabled"
+              : isSameDay(day, props.selectedDate)
+              ? "selected"
+              : format(props.currentDate, "M") !== format(day, "M")
+              ? "not-valid"
+              : "valid"
+          }`}
+          key={day}
+          onClick={() => props.onDateClick(cloneDay)}
+        >
+          <div
+            className={
+              format(props.currentDate, "M") !== format(day, "M")
+                ? "text not-valid"
+                : "valid-day"
+            }
+          >
+            {formattedDate}
+            <img src="2-b.png" alt="" />
+          </div>
+        </div>
+      );
+      day = addDays(day, 1);
+    }
+    rows.push(
+      <div className="row" key={day}>
+        {days}
+      </div>
+    );
+    days = [];
+  }
+
+  return <div className="body">{rows}</div>;
+};
+
+export default Cells;
