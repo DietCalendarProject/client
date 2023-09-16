eccb6e848549fdc60c0c0e497cfd5e173158a9b2
Jun <yuncheol.kim42@gmail.com>
2023-09-17 00:08:26 +0900

    로그인폼에서 비밀번호 인풋 분리

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,6 +5,7 @@ import "./LoginForm.css";
 import LoginButton from "../pages/login/LoginButton";
 import ErrorText from "./ErrorText";
 import EmailInput from "../pages/login/EmailInput";
+import PasswordInput from "../pages/login/PasswordInput";
 
 const LoginForm = (props) => {
   const {
@@ -14,49 +15,29 @@ const LoginForm = (props) => {
   } = useInput((value) => value.includes("@"));
 
   const {
-    value: enteredPassword,
     isValid: enteredPasswordIsValid,
-    hasError: passwordInputHasError,
-    valueChangeHandler: passwordChangeHandler,
-    inputBlurHandler: passwordBlurHandler,
     reset: resetPasswordInput,
+    ...passwordInputObject
   } = useInput((value) => value.trim() !== ""); // todo password 조건
 
   let formIsValid = false;
-
   if (enteredEmailIsValid && enteredPasswordIsValid) {
     formIsValid = true;
   }
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-
     if (!formIsValid) {
       return;
     }
     resetEmailInput();
     resetPasswordInput();
   };
-  const passwordInputClasses = passwordInputHasError
-    ? "form-control invalid"
-    : "form-control";
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <EmailInput emailInputObject={emailInputObject} />
-      <div className={passwordInputClasses}>
-        <label htmlFor="password">Your Password</label>
-        <input
-          type="password"
-          id="password"
-          onChange={passwordChangeHandler}
-          onBlur={passwordBlurHandler}
-          value={enteredPassword}
-        />
-        {passwordInputHasError && (
-          <ErrorText text="please enter a valid password" />
-        )}
-      </div>
+      <PasswordInput passwordInputObject={passwordInputObject} />
       <LoginButton $formIsValid={formIsValid} />
     </form>
   );
